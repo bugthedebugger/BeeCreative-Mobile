f9da74dc742c37ff9507fe437b0b7fc2baf4e93c
bprayush <bprayush@yahoo.com>
2019-03-01 06:49:34 +0545

    Changed API URL to production

--- lib/src/data/network/api_call.dart
+++ lib/src/data/network/api_call.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 @immutable
 class ApiURL {
-  static String _baseUrl = "https://dev.beecreative.asia/api/";
+  static String _baseUrl = "https://app.beecreative.asia/api/";
   static String login = _baseUrl + "login/google";
   static String schedules = _baseUrl + "schedules";
   static String deliveryReport = _baseUrl + "deliveryreport";
